9e6710ee2fa5e4ae62f0d589b0730b123dd2c54d
vtjavatrainner <vtpcsjavatrainner@gmail.com>
2025-06-25 09:00:13 +0530

    Added initial project for customer and processing service

---
+++ processing-service/src/main/java/com/pcs/service/processing/ProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pcs.service.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessingServiceApplication.class, args);
+	}
+
+}
